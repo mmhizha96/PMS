97b750f4c91df3e7bb401f0352676cef1dd5cb40
Michael Mhizha <mmhizha96@gmail.com>
2023-12-11 11:49:14 +0200

    cors1

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '7253b67a99b688c83ecb0c3bd5fd0677e38b0f58',
+        'reference' => 'd6d3d6c89a095b999d4717547d894e6a4408a710',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -382,7 +382,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '7253b67a99b688c83ecb0c3bd5fd0677e38b0f58',
+            'reference' => 'd6d3d6c89a095b999d4717547d894e6a4408a710',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -775,8 +775,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0|3.0',
-                1 => '3.0.0',
+                0 => '3.0.0',
+                1 => '1.0|2.0|3.0',
             ),
         ),
         'psr/simple-cache' => array(
